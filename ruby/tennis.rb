c79f298c7d262858f90b5e68e2595f3e5ad21fba
Veerpal <veerpal707@gmail.com>
2020-04-10 19:42:41 -0400

    move to smaller method

--- ruby/tennis.rb
+++ ruby/tennis.rb
@@ -9,7 +9,7 @@ class TennisGame1
   end
         
   def won_point(playerName)
-    if playerName == "player1"
+    if playerName == @player1Name
       @p1points += 1
     else
       @p2points += 1
@@ -17,44 +17,64 @@ class TennisGame1
   end
   
   def score
-    result = ""
-    tempScore=0
     if (@p1points==@p2points)
-      result = {
+      result = equalScores()
+    elsif (@p1points>=4 or @p2points>=4)
+      result = winningScores()
+    else
+      result = intermediateScores()
+    end
+    result
+  end
+
+  private
+
+  def equalScores
+    result = {
           0 => "Love-All",
           1 => "Fifteen-All",
           2 => "Thirty-All",
       }.fetch(@p1points, "Deuce")
-    elsif (@p1points>=4 or @p2points>=4)
-      minusResult = @p1points-@p2points
-      if (minusResult==1)
-        result ="Advantage player1"
-      elsif (minusResult ==-1)
-        result ="Advantage player2"
-      elsif (minusResult>=2)
-        result = "Win for player1"
-      else
-        result ="Win for player2"
-      end
+    result
+  end
+
+  def winningScores
+    minusResult = @p1points-@p2points
+    if (minusResult==1)
+      result ="Advantage player1"
+    elsif (minusResult ==-1)
+      result ="Advantage player2"
+    elsif (minusResult>=2)
+      result = "Win for player1"
     else
-      (1...3).each do |i|
-        if (i==1)
-          tempScore = @p1points
-        else
-          result+="-"
-          tempScore = @p2points
-        end
-        result += {
-            0 => "Love",
-            1 => "Fifteen",
-            2 => "Thirty",
-            3 => "Forty",
-        }[tempScore]
-      end
+      result ="Win for player2"
     end
     result
   end
+
+  def intermediateScores
+    result = ""
+    tempScore=0
+    (1...3).each do |i|
+      if (i==1)
+        tempScore = @p1points
+      else
+        result+="-"
+        tempScore = @p2points
+      end
+      result += {
+          0 => "Love",
+          1 => "Fifteen",
+          2 => "Thirty",
+          3 => "Forty",
+      }[tempScore]
+    end
+    result
+  end
+
 end
+
+
 
 class TennisGame2
   def initialize(player1Name, player2Name)
